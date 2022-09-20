42b3a77ab6eae1b31ceaf4b6b94fe75bed3ca8e9
Pardeep57 <pardeepofficial57@gmail.com>
2022-09-20 13:30:12 +0000

    final Commit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,10 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-const createTweetElement = tweetObj => {
-    const newTweet = 
-    `
+const createTweetElement = (tweetObj) => {
+  const newTweet = `
     <article>
     <header class="tweet-head">
       <div>
@@ -31,90 +29,96 @@ const createTweetElement = tweetObj => {
       </div>
     </footer>
     </article>
-    `
-    return newTweet;
-  }
-  
-  const renderTweets = arrayOfTweetObj => {
-    // instead of append we will use prepend to see newly added tweets
-    
-    for (let obj of arrayOfTweetObj) {
-      $('.tweet-container').prepend(createTweetElement(obj))
-    }
-  }
-  
-  const ajaxPost = (url, data, callback) => {
-    $.post(url, data, callback);
-  }
-  
-  const getText = queryString => {
-    let text = '';
-    for (let index in queryString) {
-      if (index > 4) {
-        text += queryString[index];
-      }
-    }
-    return decodeURIComponent(text);
-  }
-  
-  const resetErrorMessage = violation => {
-    if (violation === 'over count') {
-      $(".error-message").hide();
-      $(".error-message").empty();
-      $(".error-message").append("<p>Too many characters!</p>");
-      $(".error-message").slideDown("slow");
-    } else if (violation === 'empty') {
-      $(".error-message").hide();
-      $(".error-message").empty();
-      $(".error-message").append("<p>You wrote nothing, do you want to tweet?</p>");
-      $(".error-message").slideDown("slow");
-    } else {
-      $(".error-message").hide();
-      $(".error-message").empty();
-    }
-  };
+    `;
+  return newTweet;
+};
 
-  $(document).ready(function() {
-    $('textarea').on('input', function() {
-      this.style.height = 'auto';
-      this.style.height = this.scrollHeight + 'px';
-    });
-    //get tweets
-    $.get('/tweets', renderTweets);
-  
-    // new tweet form set as hidden
-    $('.new-tweet').hide();
-  
-    // toggle write new tweetbutton
-    $('.write').click(function(event) {
-      $('.new-tweet').slideToggle('slow');
-      $('textarea').focus();
-    });
-  
-    // button clicked = form submitted
-    $('.submit-and-display button').click(function(event) {
-      event.preventDefault();
-      const data = $('form').serialize();
-      const dataLength = getText(data).length;
-  
-      if (dataLength > 140) {
-        resetErrorMessage('over count');
-      } else if (dataLength === 0) {
-        resetErrorMessage('empty');
-      } else {
-        resetErrorMessage();
-        ajaxPost('/tweets', data, function() {
-          //Get the tweet that was just posted
-          $.get('/tweets', renderLastTweet);
-          // clear the box after tweets are posted
-          $('textarea').val("");
-        });
-        //Reset the character counter to 140 after submitting the tweet
-        $(this)
-          .closest(".new-tweet")
-          .find(".counter")
-          .removeClass("negative-count")
-          .text(140);
-      }
-    });
-  })
+const renderTweets = (arrayOfTweetObj) => {
+  // instead of append we will use prepend to see newly added tweets
+  for (let obj of arrayOfTweetObj) {
+    $(".tweet-container").prepend(createTweetElement(obj));
+  }
+};
+
+const renderLastTweet = (arrayOfTweetObj) => {
+  const lastTweet = arrayOfTweetObj[arrayOfTweetObj.length - 1];
+  $(".tweet-container").prepend(createTweetElement(lastTweet));
+};
+
+const ajaxPost = (url, data, callback) => {
+  $.post(url, data, callback);
+};
+
+const getText = (queryString) => {
+  let text = "";
+  for (let index in queryString) {
+    if (index > 4) {
+      text += queryString[index];
+    }
+  }
+  return decodeURIComponent(text);
+};
+
+const resetErrorMessage = (violation) => {
+  if (violation === "over count") {
+    $(".error-message").hide();
+    $(".error-message").empty();
+    $(".error-message").append("<p>Too many characters!</p>");
+    $(".error-message").slideDown("slow");
+  } else if (violation === "empty") {
+    $(".error-message").hide();
+    $(".error-message").empty();
+    $(".error-message").append(
+      "<p>You wrote nothing, do you want to tweet?</p>"
+    );
+    $(".error-message").slideDown("slow");
+  } else {
+    $(".error-message").hide();
+    $(".error-message").empty();
+  }
+};
+
+$(document).ready(function () {
+  $("textarea").on("input", function () {
+    this.style.height = "auto";
+    this.style.height = this.scrollHeight + "px";
+  });
+  //get method for tweets
+  $.get("/tweets", renderTweets);
+
+  // new tweet form is by default set as hidden
+  $(".new-tweet").hide();
+
+  // toggle button
+  $(".write").click(function (event) {
+    $(".new-tweet").slideToggle("slow");
+    $("textarea").focus();
+  });
+
+  //form submission When the button is clicked
+  $(".submit-and-display button").click(function (event) {
+    event.preventDefault();
+    const data = $("form").serialize();
+    const dataLength = getText(data).length;
+
+    if (dataLength > 140) {
+      resetErrorMessage("over count");
+    } else if (dataLength === 0) {
+      resetErrorMessage("empty");
+    } else {
+      resetErrorMessage();
+      ajaxPost("/tweets", data, function () {
+        //Get the tweet that was just posted
+        $.get("/tweets", renderLastTweet);
+        //Empty the textarea after submission
+        $("textarea").val("");
+      });
+      //Reset the character counter to 140 after submitting the tweet
+      $(this)
+        .closest(".new-tweet")
+        .find(".counter")
+        .removeClass("negative-count")
+        .text(140);
+    }
+  });
+});
